9a3c033aad48e26edaab774bad445a42a82ffcd0
Nischal Moktan <nischal123321@gmail.com>
2025-07-25 17:51:53 +0545

    comited

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i0cv!3)7-pw1&5ako58k9kfcxb4*a-r=pz9&*s^65&r1p7r_gv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
