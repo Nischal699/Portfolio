9d8d815449ca5503e0f20e848c28ffe20d7ee02d
Nischal Moktan <nischal123321@gmail.com>
2025-07-25 18:07:54 +0545

    commited

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,3 +141,16 @@ EMAIL_PORT=587
 EMAIL_HOST_USER='xenobaka2@gmail.com'
 EMAIL_HOST_PASSWORD='qyypewdhzscwygze'
 EMAIL_USE_TLS=True
+
+
+# Create a superuser if the environment variable is set
+from django.contrib.auth import get_user_model
+
+if os.getenv('CREATE_SUPERUSER') == 'True':
+    User = get_user_model()
+    username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
+    email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+    password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
